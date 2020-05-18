8f2175a8aee9c9395ff6686b3de9e959c718ac74
Jerry Anthony Gabriel Jacuinde <bandoler71@gmail.com>
2020-05-18 16:58:47 -0500

    Subida

---
+++ Examen2/Data/DatosContext.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace Examen2.Data
+{
+    public class DatosContext : DbContext
+    {
+        // You can add custom code to this file. Changes will not be overwritten.
+        // 
+        // If you want Entity Framework to drop and regenerate your database
+        // automatically whenever you change your model schema, please use data migrations.
+        // For more information refer to the documentation:
+        // http://msdn.microsoft.com/en-us/data/jj591621.aspx
+    
+        public DatosContext() : base("name=DatosContext")
+        {
+        }
+
+        public System.Data.Entity.DbSet<Examen2.Models.Datos> Datos { get; set; }
+    }
+}
---
+++ Examen2/Models/datos.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Examen2.Models
+{
+    public class Datos
+    {
+        public int Id { get; set; }
+        public string nombre { get; set; }
+    }
+}
